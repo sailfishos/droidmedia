6de97f46cb1afe199dbb11e8c2ec90265dd01dd2
Mohammed Hassan <mohammed.hassan@jolla.com>
2014-11-07 18:29:40 +0200

    indent header file

--- droidmediacamera.h
+++ droidmediacamera.h
@@ -10,30 +10,31 @@ extern "C" {
 
 struct DroidMediaCamera;
 
-  typedef struct {
-    int facing;
-    int orientation;
-  } DroidMediaCameraInfo;
+typedef struct {
+  int facing;
+  int orientation;
+} DroidMediaCameraInfo;
 
-  void droid_media_camera_init();
-  void droid_media_camera_deinit();
+void droid_media_camera_init();
+void droid_media_camera_deinit();
 
-  int droid_media_camera_get_number_of_cameras();
-  bool droid_media_camera_get_info(DroidMediaCameraInfo *info, int camera_number);
+int droid_media_camera_get_number_of_cameras();
+bool droid_media_camera_get_info(DroidMediaCameraInfo *info, int camera_number);
 
-  DroidMediaCamera *droid_media_camera_connect(int camera_number);
-  bool droid_media_camera_reconnect(DroidMediaCamera *camera);
-  void droid_media_camera_disconnect(DroidMediaCamera *camera);
-  bool droid_media_camera_lock(DroidMediaCamera *camera);
-  bool droid_media_camera_unlock(DroidMediaCamera *camera);
+DroidMediaCamera *droid_media_camera_connect(int camera_number);
+bool droid_media_camera_reconnect(DroidMediaCamera *camera);
+void droid_media_camera_disconnect(DroidMediaCamera *camera);
 
-  bool droid_media_camera_start_preview(DroidMediaCamera *camera);
-  void droid_media_camera_stop_preview(DroidMediaCamera *camera);
-  bool droid_media_camera_is_preview_enabled(DroidMediaCamera *camera);
+bool droid_media_camera_lock(DroidMediaCamera *camera);
+bool droid_media_camera_unlock(DroidMediaCamera *camera);
 
-  bool droid_media_camera_start_recording(DroidMediaCamera *camera);
-  void droid_media_camera_stop_recording(DroidMediaCamera *camera);
-  bool droid_media_camera_is_recording_enabled(DroidMediaCamera *camera);
+bool droid_media_camera_start_preview(DroidMediaCamera *camera);
+void droid_media_camera_stop_preview(DroidMediaCamera *camera);
+bool droid_media_camera_is_preview_enabled(DroidMediaCamera *camera);
+
+bool droid_media_camera_start_recording(DroidMediaCamera *camera);
+void droid_media_camera_stop_recording(DroidMediaCamera *camera);
+bool droid_media_camera_is_recording_enabled(DroidMediaCamera *camera);
 
 #ifdef __cplusplus
 };
