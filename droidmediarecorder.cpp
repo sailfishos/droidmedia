e6b1b4da786defe9c774efb41bc33a814a4b51b0
Andrew Branson <andrew.branson@jollamobile.com>
2018-01-19 16:49:12 +0100

    [recorder] Pass default stride and slice height parameters to the recorder. Fixes JB#40796

--- droidmediarecorder.cpp
+++ droidmediarecorder.cpp
@@ -149,8 +149,11 @@ DroidMediaRecorder *droid_media_recorder_create(DroidMediaCamera *camera, DroidM
 #else
   meta->meta_data = recorder->m_src->isMetaDataStoredInVideoBuffers();
 #endif
-  // fetch the colour format
-  recorder->m_src->getFormat()->findInt32(android::kKeyColorFormat, &meta->color_format);
+  // pass important metadata to the recorder
+  android::sp<android::MetaData> md = recorder->m_src->getFormat();
+  md->findInt32(android::kKeyStride, &meta->stride);
+  md->findInt32(android::kKeySliceHeight, &meta->slice_height);
+  md->findInt32(android::kKeyColorFormat, &meta->color_format);
 
   // Now the encoder:
 #if ANDROID_MAJOR >= 5
